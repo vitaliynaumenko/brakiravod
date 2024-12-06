9a3f58b081c91820c7be70b2592e2b93b60dcc73
vitaliynaumenko <bambiino89@gmail.com>
2024-12-06 21:38:41 +0200

    update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,10 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
+  output: 'export',
+  basePath: '/brakiravod', // Ім'я репозиторію
+  assetPrefix: '/brakiravod',
+
 };
 
 export default nextConfig;
