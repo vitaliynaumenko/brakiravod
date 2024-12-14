cdcf709aea5243bcc8b8e8b69e5a413915905087
vitaliynaumenko <bambiino89@gmail.com>
2024-12-14 15:40:26 +0200

    updated mob.vers and

--- src/pages/quizl.tsx
+++ src/pages/quizl.tsx
@@ -17,7 +17,7 @@ export default function Quizl() {
     const [country, setCountry] = useState<string>('');
     const [progressBar, setProgressBar] = useState<number | null>(null);
     const [isChecked, setIsCheked] = useState<boolean>(false);
-    // const [disable, setDisable] = useState(true);
+    const [disable, setDisable] = useState(true);
 
 
     const handelSetForm = (e: ChangeEvent<HTMLInputElement>, inputName?: string) => {
@@ -27,16 +27,15 @@ export default function Quizl() {
             [inputName ?? name]: value
 
         }))
+
+
     }
 
 
     const handleNextQuestion = () => {
         const currentQuestion = answeredQuestions + 1
         const stepWidth = (currentQuestion / (questions.length - 1)) * 100
-        console.log('stepWidth', stepWidth);
-        console.log('currentQuestion', currentQuestion);
-        console.log('stepWidth', answeredQuestions);
-        console.log('stepWidth', questions.length - 1);
+
         if (answeredQuestions < questions.length - 1) {
             setAnsweredQuestions(answeredQuestions + 1)
             setProgressBar(stepWidth)
@@ -73,22 +72,22 @@ export default function Quizl() {
 
     }, [])
 
-    // useEffect(() => {
-    //     const currentOptions = questions[answeredQuestions].options.map(option => option.name)
-    //
-    //     const isOptionsSelected = currentOptions.some(name => formState[name])
-    //
-    //     setDisable(!isOptionsSelected)
-    // }, [formState, answeredQuestions])
+    useEffect(() => {
+        const currentOptions = questions[answeredQuestions].options.map(option => option.name)
+
+        const isOptionsSelected = currentOptions.some(name => formState[name])
+
+        setDisable(!isOptionsSelected)
+    }, [formState, answeredQuestions])
 
     const onCheckedPrived = (e: ChangeEvent<HTMLInputElement>) => {
-        console.log(e.target.value)
         setIsCheked(e.target.checked)
     }
 
 
     const handleSubmit = async (e: FormEvent) => {
         e.preventDefault()
+
         try {
 
             const response = await fetch('/api/sentMessageToBot', {
@@ -105,6 +104,9 @@ export default function Quizl() {
 
         } catch (e) {
             console.error(`Error submitting form: ${e}`)
+        } finally {
+            setFormState({})
+            window.location.reload()
         }
 
 
@@ -114,52 +116,54 @@ export default function Quizl() {
         <form action="/" className={styles.quizl__form} onSubmit={handleSubmit}>
             <div className={styles.processBar} style={{width: `${progressBar}%`}}></div>
             <div className={styles.question__container}>
-                <h2 className={styles.quizl__title}>{`${questions[answeredQuestions].question} ${answeredQuestions === 1 ? switchGender(formState.divorce) : ''}`}</h2>
-                <div
-                    className={styles.quizl__desc}>{
-                    (answeredQuestions === 0)
-                        ? '(вкажіть один з варіантів відповіді)'
-                        : (questions[answeredQuestions]?.desc || '(оберіть варіант відповіді)')
-                }
-                </div>
-                <div className={styles.row}>
-                    {
-                        questions[answeredQuestions].options.map((option, index: number) => (
-                            <>
-                                {
-                                    option.name === 'tel' ? <div className={styles.inputTextWrapp}>
-                                        <PhoneInput
-                                            country={country.toLowerCase()}
-                                            onChange={(value, data, event) => handelSetForm(event, 'tel')}/></div> : (
-                                        option.name === 'city' || option.name === 'name' ?
-                                            <div className={styles.inputTextWrapp}>
-                                                <input
-                                                    type={'text'}
-                                                    name={option.name}
-                                                    value={formState[option.name]}
-                                                    checked={formState[option.name] === option.value}
-                                                    placeholder={'placeholder' in option ? option.placeholder : option.label}
-                                                    onChange={handelSetForm}
-                                                />
-                                            </div>
-                                            :
-                                            <label key={index}
-                                                   className={`${styles.label} ${formState[option.name] === option.value ? styles.labelChecked : undefined}`}>
-                                                <input
-                                                    type={option.name === 'city' || option.name === 'name' ? 'text' : 'radio'}
-                                                    name={option.name}
-                                                    value={option.value}
-                                                    checked={formState[option.name] === option.value}
-                                                    onChange={handelSetForm}
-                                                />
+                <div className={styles.containerWrapp}>
+                    <h2 className={styles.quizl__title}>{`${questions[answeredQuestions].question} ${answeredQuestions === 1 ? switchGender(formState.divorce) : ''}`}</h2>
+                    <div
+                        className={styles.quizl__desc}>{
+                        (answeredQuestions === 0)
+                            ? '(вкажіть один з варіантів відповіді)'
+                            : (questions[answeredQuestions]?.desc || '(оберіть варіант відповіді)')
+                    }
+                    </div>
+                    <div className={styles.row}>
+                        {
+                            questions[answeredQuestions].options.map((option, index: number) => (
+                                <>
+                                    {
+                                        option.name === 'tel' ? <div className={styles.inputTextWrapp}>
+                                            <PhoneInput
+                                                country={country.toLowerCase()}
+                                                onChange={(value, data, event) => handelSetForm(event, 'tel')}/>
+                                        </div> : (
+                                            option.name === 'city' || option.name === 'name' ?
+                                                <div className={styles.inputTextWrapp}>
+                                                    <input
+                                                        type={'text'}
+                                                        name={option.name}
+                                                        value={formState[option.name] || ''}
+                                                        // checked={formState[option.name] === option.value}
+                                                        placeholder={'placeholder' in option ? option.placeholder : option.label}
+                                                        onChange={handelSetForm}
+                                                    />
+                                                </div>
+                                                :
+                                                <label key={index}
+                                                       className={`${styles.label} ${formState[option.name] === option.value ? styles.labelChecked : undefined}`}>
+                                                    <input
+                                                        type={option.name === 'city' || option.name === 'name' ? 'text' : 'radio'}
+                                                        name={option.name}
+                                                        value={option.value}
+                                                        checked={formState[option.name] === option.value}
+                                                        onChange={handelSetForm}
+                                                    />
 
-                                                <span className={styles.custom__radio}></span>
-                                                <span> {option.label}</span>
-                                            </label>
-                                    )}
-                            </>
-                        ))}
-
+                                                    <span className={styles.custom__radio}></span>
+                                                    <span> {option.label}</span>
+                                                </label>
+                                        )}
+                                </>
+                            ))}
+                    </div>
                 </div>
                 {
                     answeredQuestions === questions.length - 1 && (
@@ -178,9 +182,16 @@ export default function Quizl() {
                     )
                 }
                 <div className={styles.navigation}>
-                    <button type='button' className={styles.navigationBtn} onClick={handlePrevQuestion}>prev
+                    <button type='button' className={styles.navigationBtn} onClick={handlePrevQuestion}>
+                        <svg width="15" height="16" viewBox="0 0 15 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M0.292893 7.29289C-0.0976315 7.68342 -0.0976315 8.31658 0.292893 8.70711L6.65685 15.0711C7.04738 15.4616 7.68054 15.4616 8.07107 15.0711C8.46159 14.6805 8.46159 14.0474 8.07107 13.6569L2.41421 8L8.07107 2.34315C8.46159 1.95262 8.46159 1.31946 8.07107 0.928932C7.68054 0.538408 7.04738 0.538408 6.65685 0.928932L0.292893 7.29289ZM15 7L1 7V9L15 9V7Z" fill="#101B23"/>
+                        </svg>
                     </button>
-                    <button type='button' className={styles.navigationBtn} onClick={handleNextQuestion}>next
+                    <button type='button' disabled={disable} className={styles.navigationBtn}
+                            onClick={handleNextQuestion}>
+                        <svg width="15" height="16" viewBox="0 0 15 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M14.7071 8.70711C15.0976 8.31658 15.0976 7.68342 14.7071 7.29289L8.34315 0.928932C7.95262 0.538408 7.31946 0.538408 6.92893 0.928932C6.53841 1.31946 6.53841 1.95262 6.92893 2.34315L12.5858 8L6.92893 13.6569C6.53841 14.0474 6.53841 14.6805 6.92893 15.0711C7.31946 15.4616 7.95262 15.4616 8.34315 15.0711L14.7071 8.70711ZM0 9L14 9V7L0 7L0 9Z" fill="#101B23"/>
+                    </svg>
                     </button>
                 </div>
             </div>
