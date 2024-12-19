777fbf916c02a931825f12f61c4ae7388734cf6e
vitaliynaumenko <bambiino89@gmail.com>
2024-12-19 13:06:49 +0200

    auto change step

--- src/pages/quizl.tsx
+++ src/pages/quizl.tsx
@@ -20,7 +20,7 @@ export default function Quizl() {
     const [country, setCountry] = useState<string>('');
     const [progressBar, setProgressBar] = useState<number | null>(null);
     const [isChecked, setIsCheked] = useState<boolean>(false);
-    const [disable, setDisable] = useState(true);
+    // const [disable, setDisable] = useState(true);
     const [loading, setLoading] = useState<boolean>(false);
 
     const router = useRouter()
@@ -34,22 +34,25 @@ export default function Quizl() {
 
         }))
 
-
     }
 
+    const calculationWith = (currentQuestion:number) => {
+        const stepWidth = (currentQuestion / (questions.length - 1)) * 100
+        setProgressBar(stepWidth)
+    }
 
     const handleNextQuestion = () => {
-        const currentQuestion = answeredQuestions + 1
-        const stepWidth = (currentQuestion / (questions.length - 1)) * 100
-
         if (answeredQuestions < questions.length - 1) {
-            setAnsweredQuestions(answeredQuestions + 1)
-            setProgressBar(stepWidth)
+            const currentQuestion = answeredQuestions + 1
+            setAnsweredQuestions(currentQuestion)
+            calculationWith(currentQuestion)
         }
     }
     const handlePrevQuestion = () => {
-        if (answeredQuestions !== 0) {
+        if (answeredQuestions > 0) {
+            const currentQuestion = answeredQuestions - 1
             setAnsweredQuestions(answeredQuestions - 1)
+            calculationWith(currentQuestion)
         }
     }
     const switchGender =
@@ -79,12 +82,17 @@ export default function Quizl() {
     }, [])
 
     useEffect(() => {
-        const currentOptions = questions[answeredQuestions].options.map(option => option.name)
+        if (answeredQuestions < questions.length - 1) {
 
-        const isOptionsSelected = currentOptions.some(name => formState[name])
+            const currentOptions = questions[answeredQuestions].options.map(option => option.name)
 
-        setDisable(!isOptionsSelected)
-    }, [formState, answeredQuestions])
+            const isOptionsSelected = currentOptions.some(name => formState[name])
+            if (isOptionsSelected) {
+                handleNextQuestion()
+            }
+        }
+
+    }, [formState])
 
     const onCheckedPrived = (e: ChangeEvent<HTMLInputElement>) => {
         setIsCheked(e.target.checked)
@@ -146,7 +154,7 @@ export default function Quizl() {
                         <div className={styles.row}>
                             {
                                 questions[answeredQuestions].options.map((option, index: number) => (
-                                    <div key={index}>
+                                    <>
                                         {
                                             option.name === 'tel' ? <div className={styles.inputTextWrapp}>
                                                 <PhoneInput
@@ -159,7 +167,6 @@ export default function Quizl() {
                                                             type={'text'}
                                                             name={option.name}
                                                             value={formState[option.name] || ''}
-                                                            // checked={formState[option.name] === option.value}
                                                             placeholder={'placeholder' in option ? option.placeholder : option.label}
                                                             onChange={handelSetForm}
                                                         />
@@ -179,7 +186,7 @@ export default function Quizl() {
                                                         <span> {option.label}</span>
                                                     </label>
                                             )}
-                                    </div>
+                                    </>
                                 ))}
                         </div>
                         {
@@ -187,11 +194,13 @@ export default function Quizl() {
                                 <>
                                     <label className={styles.labelPolicy}>
                                         <input type="checkbox" onChange={onCheckedPrived}/>
-                                        <span className={styles.prived__policy}> Я погоджуюсь з <Link href={'/policy'}
-                                                                                                      target='_blank'
-                                                                                                      className={styles.policy__link}>політикою конфіденційності та правилами використання *</Link></span>
+                                        <span className={styles.prived__policy}> Я погоджуюсь з
+                                            <Link href={'/policy'}
+                                                  target='_blank'
+                                                  className={styles.policy__link}>політикою конфіденційності та правилами використання *</Link></span>
                                     </label>
-                                    <button type='submit' disabled={!isChecked} className={styles.btnSubmit}>Замовити
+                                    <button type='submit' disabled={!isChecked} className={styles.btnSubmit}>
+                                        Замовити
                                     </button>
                                     <div className={styles.socialMedia}>
                                         <div className={styles.socialMediaTitle}>
@@ -205,27 +214,32 @@ export default function Quizl() {
                         }
                     </div>
 
+                    {
+                        answeredQuestions < questions.length - 1 && (
+                            <div className={styles.navigation}>
+                                <button type='button' disabled={answeredQuestions === 0} className={styles.navigationBtn}
+                                        onClick={handlePrevQuestion}>
+                                    <svg width="15" height="16" viewBox="0 0 15 16" fill="none"
+                                         xmlns="http://www.w3.org/2000/svg">
+                                        <path
+                                            d="M0.292893 7.29289C-0.0976315 7.68342 -0.0976315 8.31658 0.292893 8.70711L6.65685 15.0711C7.04738 15.4616 7.68054 15.4616 8.07107 15.0711C8.46159 14.6805 8.46159 14.0474 8.07107 13.6569L2.41421 8L8.07107 2.34315C8.46159 1.95262 8.46159 1.31946 8.07107 0.928932C7.68054 0.538408 7.04738 0.538408 6.65685 0.928932L0.292893 7.29289ZM15 7L1 7V9L15 9V7Z"
+                                            fill="#101B23"/>
+                                    </svg>
+                                </button>
+                                <button type='button' className={styles.navigationBtn}
+                                        onClick={handleNextQuestion}>
+                                    <svg width="15" height="16" viewBox="0 0 15 16" fill="none"
+                                         xmlns="http://www.w3.org/2000/svg">
+                                        <path
+                                            d="M14.7071 8.70711C15.0976 8.31658 15.0976 7.68342 14.7071 7.29289L8.34315 0.928932C7.95262 0.538408 7.31946 0.538408 6.92893 0.928932C6.53841 1.31946 6.53841 1.95262 6.92893 2.34315L12.5858 8L6.92893 13.6569C6.53841 14.0474 6.53841 14.6805 6.92893 15.0711C7.31946 15.4616 7.95262 15.4616 8.34315 15.0711L14.7071 8.70711ZM0 9L14 9V7L0 7L0 9Z"
+                                            fill="#101B23"/>
+                                    </svg>
+                                </button>
+                            </div>
+                        )
 
-                    <div className={styles.navigation}>
-                        <button type='button' disabled={answeredQuestions === 0} className={styles.navigationBtn}
-                                onClick={handlePrevQuestion}>
-                            <svg width="15" height="16" viewBox="0 0 15 16" fill="none"
-                                 xmlns="http://www.w3.org/2000/svg">
-                                <path
-                                    d="M0.292893 7.29289C-0.0976315 7.68342 -0.0976315 8.31658 0.292893 8.70711L6.65685 15.0711C7.04738 15.4616 7.68054 15.4616 8.07107 15.0711C8.46159 14.6805 8.46159 14.0474 8.07107 13.6569L2.41421 8L8.07107 2.34315C8.46159 1.95262 8.46159 1.31946 8.07107 0.928932C7.68054 0.538408 7.04738 0.538408 6.65685 0.928932L0.292893 7.29289ZM15 7L1 7V9L15 9V7Z"
-                                    fill="#101B23"/>
-                            </svg>
-                        </button>
-                        <button type='button' disabled={disable} className={styles.navigationBtn}
-                                onClick={handleNextQuestion}>
-                            <svg width="15" height="16" viewBox="0 0 15 16" fill="none"
-                                 xmlns="http://www.w3.org/2000/svg">
-                                <path
-                                    d="M14.7071 8.70711C15.0976 8.31658 15.0976 7.68342 14.7071 7.29289L8.34315 0.928932C7.95262 0.538408 7.31946 0.538408 6.92893 0.928932C6.53841 1.31946 6.53841 1.95262 6.92893 2.34315L12.5858 8L6.92893 13.6569C6.53841 14.0474 6.53841 14.6805 6.92893 15.0711C7.31946 15.4616 7.95262 15.4616 8.34315 15.0711L14.7071 8.70711ZM0 9L14 9V7L0 7L0 9Z"
-                                    fill="#101B23"/>
-                            </svg>
-                        </button>
-                    </div>
+                    }
+
                 </div>
 
             </form>
